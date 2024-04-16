09fafb60f74e97b01f03c1bae8af0c071fbed269
Carlos Garcia <cagarciagarcia@gmail.com>
2024-04-16 14:51:36 +0200

    commit a ver si sube a git desde local a remoto....???

--- proyecto1Git/src/principal/Test.java
+++ proyecto1Git/src/principal/Test.java
@@ -1,7 +1,7 @@
 package principal;
 
 public class Test {
-
+	String prueba="a ver si sube a git desde local a remoto....???"
 	public Test() {
 		super();
 		// TODO Auto-generated constructor stub
