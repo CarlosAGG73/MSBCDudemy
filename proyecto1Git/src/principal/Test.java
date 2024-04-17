e4d1096ef1200f8cd54d7258edd24844aa51c68e
Carlos Garcia <cagarciagarcia@gmail.com>
2024-04-17 11:04:07 +0200

    prueba con token github

--- proyecto1Git/src/principal/Test.java
+++ proyecto1Git/src/principal/Test.java
@@ -1,7 +1,8 @@
 package principal;
 
 public class Test {
-	String prueba="a ver si sube a git desde local a remoto....???"
+	String prueba="a ver si sube a git desde local a remoto....???";
+	String prueba2="prueba con token github";
 	public Test() {
 		super();
 		// TODO Auto-generated constructor stub
